c88892c1ed9739ecd72b596c028649197c239261
Akaaljot Singh <akaaljotsingh@gmail.com>
2018-08-08 17:27:03 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pwa';
+  update:boolean = false;
+  joke: any;
+
+  constructor(updates:SwUpdate, private data:DataService) {
+    updates.available.subscribe(event => {
+      updates.activateUpdate().then(()=>document.location.reload());
+    })
+  }
+
+  ngOnInit() {
+    this.data.gimmyJokes().subscribe(res => {
+      this.joke = res;
+    });
+  }
+
 }
